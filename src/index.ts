754bdbdacdd824ba4c375a7eee323801d800dffd
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2025-12-15 12:19:06 +0100

    Changed code so it updated uuid

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
 import dotenv from 'dotenv';
 import { GamerBotAPI } from 'gamerbot-module';
-import { getProfileFromUUID } from 'minecraft-api-wrapper';
+import { getProfilesFromUsernames } from 'minecraft-api-wrapper';
 dotenv.config();
+
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 (async () => {
     const gamerbotInstance = new GamerBotAPI(process.env.GAMERBOT_TOKEN!);
 
     const minecraftUsers = await gamerbotInstance.models.getAllMinecraftUsers();
 
+    const names = []
     for (const user of minecraftUsers) {
-        const minecraftProfile = await getProfileFromUUID(user.uuid);
-        console.log(`Fetched profile for UUID ${user.uuid}:`);
-        if (minecraftProfile && minecraftProfile.getName() !== user.name) {
-            console.log(`Updating username for UUID ${user.uuid} from ${user.name} to ${minecraftProfile.getName()}`);
+        names.push(user.name);
+    } 
 
-            const userData = await gamerbotInstance.models.getUserData(user.discordId);
-            userData.minecraftData.username = minecraftProfile.getName();
-            await userData.save();
-        }
+    console.log(`Fetching UUIDs for ${names.length} Minecraft users...`);
+    const minecraftProfiles = await getProfilesFromUsernames(names);
+    console.log(`Fetched ${minecraftProfiles?.length || 0} profiles.`);
+
+    if (!minecraftProfiles) {
+        console.log('No Minecraft profiles found.');
+        return;
     }
+
+    for (const profile of minecraftProfiles) {
+        console.log(`Updating UUID for ${profile.getName()} to ${profile.getUUID()}`);
+        const user = minecraftUsers.find(u => u.name.toLowerCase() === profile.getName().toLowerCase());
+        const userData = await gamerbotInstance.models.getUserData(user!.discordId);
+        userData.minecraftData.uuid = profile.getUUID();
+        await userData.save();
+        console.log(`Updated UUID for ${profile.getName()}`);
+    }
+
 })();
+
